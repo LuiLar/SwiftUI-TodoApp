48b507587f82b1715069c70566fc0832e32924b1
Luis Larghi <windinglair@gmail.com>
2025-01-30 15:56:45 -0400

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Luis on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
