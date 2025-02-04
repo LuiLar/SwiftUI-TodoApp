8201ed631982aa9808dda95634e965c142c20a28
Luis Larghi <windinglair@gmail.com>
2025-02-04 17:49:34 -0400

    Add collapsable sections for completed and non-completed

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -14,42 +14,82 @@ struct ContentView: View {
     @State private var isShowingNewTaskSheet = false
     @State private var currentTask: String = ""
     @State private var isEditMode: EditMode = .inactive
+    @State private var isInProgressExpanded: Bool = true
+    @State private var isCompletedExpanded: Bool = true
 
-    @Query(sort: \Task.orderIndex) var tasks: [Task]
+    @Query(filter: #Predicate<Task> { task in
+        task.isCompleted == false
+    }, sort: \Task.orderIndex) var inProgressTasks: [Task]
+    
+    @Query(filter: #Predicate<Task> { task in
+        task.isCompleted == true
+    }, sort: \Task.orderIndex) var completedTasks: [Task]
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(tasks) { task in
-                    TaskItemView(data: task, isEditMode: isEditMode == .active ? true : false)
+                if !inProgressTasks.isEmpty {
+                    Section(
+                        isExpanded: $isInProgressExpanded,
+                        content: {
+                            ForEach(inProgressTasks) { task in
+                                TaskItemView(data: task, isEditMode: isEditMode == .active ? true : false)
+                            }
+                            .onMove { (indexSet, toIndex) in
+                                for fromIndex in indexSet {
+                                    inProgressTasks[fromIndex].orderIndex = toIndex - 1
+                                    inProgressTasks[toIndex].orderIndex = fromIndex
+                                }
+                            }
+                            .onDelete { indexSet in
+                                for index in indexSet {
+                                    context.delete(inProgressTasks[index])
+                                }
+                            }
+                        },
+                        header: { Text("In Progress") }
+                    )
                 }
-                .onMove { (indexSet, toIndex) in
-                    for fromIndex in indexSet {
-                        tasks[fromIndex].orderIndex = toIndex - 1
-                        tasks[toIndex].orderIndex = fromIndex
-                    }
-                }
-                .onDelete { indexSet in
-                    for index in indexSet {
-                        context.delete(tasks[index])
-                    }
+                
+                if !completedTasks.isEmpty {
+                    Section(
+                        isExpanded: $isCompletedExpanded,
+                        content: {
+                            ForEach(completedTasks) { task in
+                                TaskItemView(data: task, isEditMode: isEditMode == .active ? true : false)
+                            }
+                            .onMove { (indexSet, toIndex) in
+                                for fromIndex in indexSet {
+                                    completedTasks[fromIndex].orderIndex = toIndex - 1
+                                    completedTasks[toIndex].orderIndex = fromIndex
+                                }
+                            }
+                            .onDelete { indexSet in
+                                for index in indexSet {
+                                    context.delete(completedTasks[index])
+                                }
+                            }
+                        },
+                        header: { Text("Completed") }
+                    )
                 }
             }
             .navigationTitle("Tasks")
-            .sheet(isPresented: $isShowingNewTaskSheet) { NewTaskView(itemsCount: tasks.count) }
+            .listStyle(.sidebar)
+            .sheet(isPresented: $isShowingNewTaskSheet) { NewTaskView(itemsCount: inProgressTasks.count) }
             .toolbar {
                 ToolbarItemGroup(placement: .topBarLeading) {
-                    if !tasks.isEmpty { EditButton() }
+                    if !inProgressTasks.isEmpty { EditButton() }
                 }
                 
                 ToolbarItemGroup(placement: .topBarTrailing) {
-                    if !tasks.isEmpty {
+                    if !inProgressTasks.isEmpty {
                         Button("Add Task", systemImage: "plus") { isShowingNewTaskSheet.toggle() }
                     }
                 }
             }
             .overlay {
-                if tasks.isEmpty {
+                if inProgressTasks.isEmpty && completedTasks.isEmpty {
                     ContentUnavailableView(label: {
                         Label("No tasks", systemImage: "pencil.and.list.clipboard")
                     }, description: {
